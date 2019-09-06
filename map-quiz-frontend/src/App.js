1092436b5c53c319613621fa7ac82554ec4a0353
Michelle Lam <shelly2.lam@gmail.com>
2019-09-06 17:05:45 -0400

    added notes

--- map-quiz-frontend/src/App.js
+++ map-quiz-frontend/src/App.js
@@ -4,11 +4,17 @@ import Map from "./components/Map.js";
 import APIKEY from "./APIKEY.js";
 
 function App() {
+
+
   return (
     <div className="App">
+      
+   
+     
       <Map
+     
         isMarkerShown={false}
-        googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${APIKEY}`}
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${APIKEY}&libraries=geometry`}
         loadingElement={<div style={{ height: `100%` }} />}
         containerElement={<div style={{ height: `800px` }} />}
         mapElement={<div style={{ height: `100%` }} />}
--- map-quiz-frontend/src/components/Map.js
+++ map-quiz-frontend/src/components/Map.js
@@ -7,22 +7,47 @@ import {
 } from "react-google-maps";
 import MapOptions from "../MapOptions.json";
 
+const google = window.google
+// reminders for resources we can use to calculate distance between the click and city coordinates
+let arr = [{lat: 48.8667, lng: 2.3333}, {lat: 55.7522, lng: 37.6155}, {lat: -22.925, lng: -43.225}]
+
+// google.maps.geometry.spherical.computeDistanceBetween(LatLng1, myLatLng)
+
+const createMarker = () =>
+    new window.google.maps.Marker({
+      position: { lat: 43.642567, lng: -79.387054 },
+      map: this.googleMap,
+    })
+
+  let markP = arr[0]
+  let i = 1
 const handleClick = (e) => {
   console.log(e)
   console.log(e.latLng)
+  markP = arr[i]
   debugger
+  i += 1
 }
+
+
+
+
+// {myLatLng = new google.maps.LatLng({lat: -34, lng: 151})}
 
 const Map = withScriptjs(
   withGoogleMap(props => (
-    <GoogleMap
+    <React.Fragment>
+      
+      <GoogleMap
       defaultZoom={2.3}
       defaultCenter={{ lat: 25, lng: 0 }}
       defaultOptions={{ styles: MapOptions, maxZoom: 7, minZoom: (2.3), gestureHandling: 'cooperative'}}
       onClick={handleClick}
-    >
-      {/* {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />} */}
-    </GoogleMap>
+      >
+        {<Marker position={markP} />} */}
+      </GoogleMap>
+    </React.Fragment>
+   
   ))
 );
 
