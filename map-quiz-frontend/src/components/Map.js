d859daa10319443c4f1564f24a421f87cdae6592
Michelle Lam <shelly2.lam@gmail.com>
2019-09-06 14:45:06 -0400

    latlang

--- map-quiz-frontend/src/components/Map.js
+++ map-quiz-frontend/src/components/Map.js
@@ -7,12 +7,19 @@ import {
 } from "react-google-maps";
 import MapOptions from "../MapOptions.json";
 
+const handleClick = (e) => {
+  console.log(e)
+  console.log(e.latLng)
+  debugger
+}
+
 const Map = withScriptjs(
   withGoogleMap(props => (
     <GoogleMap
       defaultZoom={2.3}
       defaultCenter={{ lat: 25, lng: 0 }}
-      defaultOptions={{ styles: MapOptions, maxZoom: 7, minZoom: 2.3 }}
+      defaultOptions={{ styles: MapOptions, maxZoom: 7, minZoom: (2.3), gestureHandling: 'cooperative'}}
+      onClick={handleClick}
     >
       {/* {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />} */}
     </GoogleMap>
