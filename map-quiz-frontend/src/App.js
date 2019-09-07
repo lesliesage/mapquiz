df7c1b58edf96c8b4a40244a83727dc5f08ac8ea
lesliesage <leslie.sage@gmail.com>
2019-09-07 16:20:07 -0400

    added routes and installed semantic

--- map-quiz-frontend/src/App.js
+++ map-quiz-frontend/src/App.js
@@ -2,25 +2,23 @@ import React from "react";
 import "./App.css";
 import NavBar from "./components/NavBar.js";
 import LoginContainer from "./containers/LoginContainer.js";
+import Login from "./components/Login.js";
 import QuizContainer from "./containers/QuizContainer.js";
 import StatsContainer from "./containers/StatsContainer.js";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-
-
-class App extends React.Component {
-
-  render() {
-    return (
-      <div className="App">
+const App = () => {
+  return (
+    <Router>
+      <React.Fragment>
         <NavBar />
-        <LoginContainer />
-        <QuizContainer />
-        <StatsContainer />
-        
-      </div>
-    );
-
-  }
-}
+        <Route exact path="/" component={LoginContainer} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/play" component={QuizContainer} />
+        <Route exact path="/stats" component={StatsContainer} />
+      </React.Fragment>
+    </Router>
+  );
+};
 
 export default App;
