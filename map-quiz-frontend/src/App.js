89715e780cf250a205afc57653aa403c3957052b
lesliesage <leslie.sage@gmail.com>
2019-09-09 09:44:12 -0400

    modalizing login

--- map-quiz-frontend/src/App.js
+++ map-quiz-frontend/src/App.js
@@ -1,33 +1,39 @@
 import React from "react";
 import "./App.css";
 import NavBar from "./components/NavBar.js";
-import LoginContainer from "./containers/LoginContainer.js";
+import Splash from "./containers/Splash.js";
 import Login from "./components/Login.js";
 import QuizContainer from "./containers/QuizContainer.js";
 import StatsContainer from "./containers/StatsContainer.js";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
-
-
 class App extends React.Component {
+  state = {
+    hidden: true
+  };
 
+  handleLoginClick = () => {
+    this.setState({ hidden: !this.state.hidden });
+  };
 
   render() {
     return (
       <div className="App">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/1.11.8/semantic.min.css"/>
-    <Router>
-      <React.Fragment>
-        <NavBar />
-        <Route exact path="/" component={LoginContainer} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/play" component={QuizContainer} />
-        <Route exact path="/stats" component={StatsContainer} />
-      </React.Fragment>
-    </Router>
-    </div>
-  );
-};
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/1.11.8/semantic.min.css"
+        />
+        <Router>
+          <React.Fragment>
+            <NavBar handleLoginClick={this.handleLoginClick} />
+            <Route exact path="/" component={Splash} />
+            <Route exact path="/play" component={QuizContainer} />
+            <Route exact path="/stats" component={StatsContainer} />
+          </React.Fragment>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ map-quiz-frontend/src/containers/Splash.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import FormContainer from './FormContainer'
+
+class Splash extends Component {
+
+  render() {
+    return <div>Splash Here
+      <FormContainer />
+    </div>;
+  }
+}
+
+export default Splash;
