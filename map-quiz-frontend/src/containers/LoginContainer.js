339433852374646a780e38a92ac85b3e34a8f255
Michelle Lam <shelly2.lam@gmail.com>
2019-09-09 09:44:43 -0400

    changed

--- map-quiz-frontend/src/containers/LoginContainer.js
+++ map-quiz-frontend/src/containers/LoginContainer.js
@@ -2,10 +2,8 @@ import React, { Component } from "react";
 import FormContainer from './FormContainer'
 
 class LoginContainer extends Component {
-  state = {
-    hidden: true
-  };
-  
+ 
+
   render() {
     return <div>LoginContainer Here
       <FormContainer />
