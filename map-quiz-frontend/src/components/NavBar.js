7614e81c2f95a8c4fa49a9051fff7aa1f7a07698
lesliesage <leslie.sage@gmail.com>
2019-09-10 08:48:38 -0400

    everything is terrible

--- map-quiz-frontend/src/components/NavBar.js
+++ map-quiz-frontend/src/components/NavBar.js
@@ -1,21 +1,30 @@
-import React from 'react';
+  
+import PropTypes from 'prop-types'
 import { NavLink } from 'react-router-dom';
-import { Button, Container } from 'semantic-ui-react'
-
-const NavBar = (props) => {
-  return (
-    <Container className="navbar">
-        <Button><NavLink exact to="/">MapQuiz</NavLink></Button>
-        {/* <Button><NavLink exact to="/">Home</NavLink></Button> */}
-      {props.user ? 
-        <React.Fragment><Button><NavLink exact to="/play">Play</NavLink></Button><Button><NavLink exact to="/stats">Stats</NavLink></Button><Button style={{color: "#009fda"}} onClick={props.handleLoginClick}>Logout</Button></React.Fragment> : <Button style={{color: "#009fda"}} onClick={props.handleLoginClick}>Login</Button>
-      }
-        
-        
-    </Container>
-  );
-};
+import React, { Component } from 'react'
+import {
+  Button,
+  Menu
+} from 'semantic-ui-react'
 
 
+class NavBar extends Component {
+  render() {
+    return (
+        <Menu id="navbar" size='large'>
+            <Menu.Item id="mapquiz-link" as='a' active><NavLink exact to="/">MapQuiz</NavLink></Menu.Item>
+            {this.props.user ? (
+              <React.Fragment>
+                <Menu.Item id="play-link" as='a'><NavLink exact to="/play">Play</NavLink></Menu.Item>
+                <Menu.Item id="stats-link" as='a'><NavLink exact to="/stats">Stats</NavLink></Menu.Item>
+                <Button as='a' style={{ marginLeft: '0.5em' }} onClick={this.props.handleLoginClick}>Logout</Button>
+              </React.Fragment>
+            ) : (
+              <Button id="login-button" as='a' style={{ marginLeft: '0.5em' }} onClick={this.props.handleLoginClick}>Login</Button>
+            )}
+        </Menu>
+    )       
+  }
+}
 
 export default NavBar;
