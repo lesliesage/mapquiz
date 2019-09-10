998853bcb7aed5d0ae5e12a5a5d546e07945a80b
lesliesage <leslie.sage@gmail.com>
2019-09-10 18:44:13 -0400

    sorted games list

--- map-quiz-frontend/src/containers/Deets.js
+++ map-quiz-frontend/src/containers/Deets.js
@@ -2,16 +2,15 @@ import React, { Component } from "react";
 import { Grid } from "semantic-ui-react";
 import Question from "../components/Question.js";
 
-
 class Deets extends Component {
-  state = {};
   render() {
     return (
       <Grid id="deets">
-          {this.props.game ? 
-          this.props.game.questions.map((question, index) => <Question key={question.id} question={question} index={index} />
-          ) : null
-        }
+        {this.props.game
+          ? this.sorted().map((question, index) => (
+              <Question key={question.id} question={question} index={index} />
+            ))
+          : null}
       </Grid>
     );
   }
--- map-quiz-frontend/src/containers/MyStats.js
+++ map-quiz-frontend/src/containers/MyStats.js
@@ -3,11 +3,13 @@ import Game from "../components/Game.js";
 import { Container, Header } from "semantic-ui-react";
 
 class MyStats extends Component {
-  state = {};
+  sorted = () => {
+    return this.props.games.sort((a, b) => (a.score < b.score ? 1 : -1));
+  };
   render() {
     return (
       <Container id="my-stats-section">
-        {this.props.games.map(game => {
+        {this.sorted().map(game => {
           return (
             <Game
               key={game.id}
