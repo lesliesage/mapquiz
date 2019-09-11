254c2d5af8106a9dc5201286be16c5d22fb24b1f
lesliesage <leslie.sage@gmail.com>
2019-09-10 21:57:40 -0400

    allow zoom on scroll

--- map-quiz-frontend/src/components/Map.js
+++ map-quiz-frontend/src/components/Map.js
@@ -7,7 +7,6 @@ import {
 } from "react-google-maps";
 import MapOptions from "../MapOptions.json";
 
-
 // reminders for resources we can use to calculate distance between the click and city coordinates
 let arr = [
   { lat: 48.8667, lng: 2.3333 },
@@ -23,36 +22,42 @@ let arr = [
 //     map: this.googleMap
 //   });
 
-
-
 // {myLatLng = new google.maps.LatLng({lat: -34, lng: 151})}
 let handleClick = (e, props) => {
-  props.toggleMarker()
-  let latLng = e.latLng
-  props.setChoice(latLng)
-  let mylatlng = new window.google.maps.LatLng({lat: props.currentCity.lat, lng: props.currentCity.long})
-  let distance = window.google.maps.geometry.spherical.computeDistanceBetween(latLng, mylatlng)/10000
-  console.log(distance)
+  props.toggleMarker();
+  let latLng = e.latLng;
+  props.setChoice(latLng);
+  let mylatlng = new window.google.maps.LatLng({
+    lat: props.currentCity.lat,
+    lng: props.currentCity.long
+  });
+  let distance =
+    window.google.maps.geometry.spherical.computeDistanceBetween(
+      latLng,
+      mylatlng
+    ) / 10000;
+  console.log(distance);
   if (props.currentScore - distance >= 0) {
-    props.setScore(distance)
-    props.toggleNextButton()
-    let formattedQuestion = {distance: distance, city_id: props.currentCity.id}
-    props.addQuestion(formattedQuestion)
-   
-    if(props.cityIndex >= 19){
-      
-      props.createGame()
-      props.showScoreModal()
+    props.setScore(distance);
+    props.toggleNextButton();
+    let formattedQuestion = {
+      distance: distance,
+      city_id: props.currentCity.id
+    };
+    props.addQuestion(formattedQuestion);
+
+    if (props.cityIndex >= 19) {
+      props.createGame();
+      props.showScoreModal();
     }
   } else {
-    props.showModal()
+    props.showModal();
   }
-}
+};
 
-let formatlatlng = (currentCity) => {
-  return  {lat: currentCity.lat, lng: currentCity.long}
-  
-}
+let formatlatlng = currentCity => {
+  return { lat: currentCity.lat, lng: currentCity.long };
+};
 
 const Map = withScriptjs(
   withGoogleMap(props => (
@@ -65,12 +70,26 @@ const Map = withScriptjs(
           styles: MapOptions,
           maxZoom: 7,
           minZoom: 2.3,
-          gestureHandling: "cooperative"
+          gestureHandling: "greedy"
         }}
-        onClick={(e) => handleClick(e, props)}
+        onClick={e => handleClick(e, props)}
       >
-        {props.isMarkerShown && props.currentCity ? <Marker icon={{                               url: "http://maps.google.com/mapfiles/ms/icons/red-dot.png"                           }} position={formatlatlng(props.currentCity)}/> : null}
-        {props.yourChoice ? <Marker icon={{url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"}}position={ props.yourChoice }/> : null}
+        {props.isMarkerShown && props.currentCity ? (
+          <Marker
+            icon={{
+              url: "http://maps.google.com/mapfiles/ms/icons/red-dot.png"
+            }}
+            position={formatlatlng(props.currentCity)}
+          />
+        ) : null}
+        {props.yourChoice ? (
+          <Marker
+            icon={{
+              url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"
+            }}
+            position={props.yourChoice}
+          />
+        ) : null}
       </GoogleMap>
     </React.Fragment>
   ))
